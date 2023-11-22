cb511976d69ab65086be617a5042b02f3251090f
gwansikk <seorit98@gmail.com>
2023-11-22 17:10:56 +0900

    feat: input

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
+
+import Input from "./Input";
+
+describe("Running Test for Input", () => {
+  test("Check placeholder in Input", () => {
+    render(<Input placeholder="Hello" />);
+    expect(screen.getByPlaceholderText("Hello")).toHaveAttribute("placeholder", "Hello");
+  });
+
+  test("renders the Input component", async () => {
+    render(<Input placeholder="Input" />);
+    const input = screen.getByPlaceholderText("Input") as HTMLInputElement;
+    userEvent.type(input, "Hello world!");
+    await waitFor(() => expect(input.value).toBe("Hello world!"));
+  });
+});
