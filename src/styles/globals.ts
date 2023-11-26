37b8d50f27ac1e9b53d6fec6875b6b2433e7fdb1
gwansikk <seorit98@gmail.com>
2023-11-26 23:35:47 +0900

    feat: add global style

---
+++ src/styles/globals.ts
@@ -0,0 +1,14 @@
+export const backgroundColor = {
+  light: "#1ea7fd",
+  dark: "#4D6B7D",
+  success: "#1EFC5D",
+  error: "#FC1E1F",
+  warning: "#FCBB1E",
+};
+
+export const padding = {
+  sm: "8px 24px",
+  md: "12px 28px",
+  lg: "16px 32px",
+  full: "12px 28px",
+};
