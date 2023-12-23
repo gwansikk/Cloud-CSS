d23c7d28b7904e3362f0b76e909644d8a1aa9f61
gwansikk <seorit98@gmail.com>
2023-12-23 15:33:39 +0900

    feat: global styles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,14 +1,12 @@
-export const backgroundColor = {
+export const globalStyleColor = {
   light: "#1ea7fd",
   dark: "#4D6B7D",
   success: "#1EFC5D",
   error: "#FC1E1F",
-  warning: "#FCBB1E",
 };
 
-export const padding = {
+export const globalStylePadding = {
   sm: "8px 24px",
   md: "12px 28px",
   lg: "16px 32px",
-  full: "12px 28px",
 };
