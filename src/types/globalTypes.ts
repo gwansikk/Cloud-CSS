effc2ec64e60f5f8f2a0468abd9b2e13064a5d28
gwansikk <seorit98@gmail.com>
2023-12-23 15:33:25 +0900

    feat: global types

---
+++ src/types/globalTypes.ts
@@ -0,0 +1,4 @@
+export type GlobalTypeColor = "light" | "dark";
+export type GlobalTypeActivate = "normal" | "disabled";
+export type GlobalTypeStatus = "success" | "error";
+export type GlobalTypeSize = "sm" | "md" | "lg";
