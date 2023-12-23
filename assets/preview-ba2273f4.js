9740b6e0ce3dbfad4f32b7dcafb088d212a4b1de
gwansikk <39869096+gwansikk@users.noreply.github.com>
2023-12-23 06:36:08 +0000

    Deploying to gh-pages from @ gwansikk/GooreumUI@f3350ad1e07dcb7db036e76cb0307d190176940e 🚀

--- assets/preview-ba2273f4.js
+++ assets/preview-ba2273f4.js
@@ -1,2 +1,1 @@
 const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}}};export{e as default};
-//# sourceMappingURL=preview-ba2273f4.js.map
