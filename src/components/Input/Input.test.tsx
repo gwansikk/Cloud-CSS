0c617d2cf0c5a993b97183cdfeb5cee18b609b30
gwansikk <seorit98@gmail.com>
2023-12-24 19:35:44 +0900

    feat: Input test

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,20 +1,53 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Input from "./Input";
 
 describe("Running Test for Input", () => {
-  test("Check placeholder in Input", () => {
-    render(<Input placeholder="Hello" />);
-    expect(screen.getByPlaceholderText("Hello")).toHaveAttribute("placeholder", "Hello");
+  describe("when label", () => {
+    it("should have label", () => {
+      render(<Input label="label" />);
+      expect(screen.getByText("label")).toBeInTheDocument();
+    });
   });
 
-  test("renders the Input component", async () => {
-    render(<Input placeholder="Input" />);
-    const input = screen.getByPlaceholderText("Input") as HTMLInputElement;
-    userEvent.type(input, "Hello world!");
-    await waitFor(() => expect(input.value).toBe("Hello world!"));
+  describe("Check placeholder", () => {
+    it("should be placeholder", () => {
+      render(<Input placeholder="Hello" />);
+      expect(screen.getByPlaceholderText("Hello")).toBeInTheDocument();
+    });
+  });
+
+  describe("when disabled", () => {
+    it("should be disabled", () => {
+      render(<Input status="disabled" placeholder="Disabled Input" />);
+      const inputElement = screen.getByPlaceholderText("Disabled Input");
+      expect(inputElement).toBeDisabled();
+    });
+  });
+
+  describe("when in success state", () => {
+    it("should display success message", () => {
+      const successMessage = "success message";
+      render(<Input status="success" message={successMessage} />);
+      expect(screen.getByText(successMessage)).toBeInTheDocument();
+    });
+  });
+
+  describe("when in error state", () => {
+    it("should display error message", () => {
+      const errorMessage = "Error message";
+      render(<Input status="error" message={errorMessage} />);
+      expect(screen.getByText(errorMessage)).toBeInTheDocument();
+    });
+  });
+
+  describe("when readonly", () => {
+    it("should be readonly", () => {
+      render(<Input readOnly placeholder="Readonly Input" />);
+      const inputElement = screen.getByPlaceholderText("Readonly Input");
+      expect(inputElement).toHaveAttribute("readonly");
+    });
   });
 });
