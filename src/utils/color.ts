486e1d40587d96186e7ca36dc2786900ac860078
gwansikk <seorit98@gmail.com>
2024-01-08 19:03:49 +0900

    refactor(utils): 관심사 별 함수 합치기

---
+++ src/utils/color.ts
@@ -0,0 +1,36 @@
+import theme from "@styles/theme";
+import type { GlobalTypeActivate, GlobalTypeStatus } from "@type/globalTypes";
+
+export const selectBackgroundColorForStatus = (
+  status?: GlobalTypeActivate | GlobalTypeStatus,
+): string => {
+  switch (status) {
+    case "disabled":
+      return "#e4e3ea";
+
+    case "error":
+      return theme.backgroundColor.red;
+
+    case "success":
+      return theme.backgroundColor.green;
+
+    default:
+      return theme.backgroundColor.grey;
+  }
+};
+
+export const selectColorForStatus = (status?: GlobalTypeActivate | GlobalTypeStatus): string => {
+  switch (status) {
+    case "disabled":
+      return "#e4e3ea";
+
+    case "error":
+      return theme.color.red;
+
+    case "success":
+      return theme.color.green;
+
+    default:
+      return theme.color.default;
+  }
+};
--- src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-// utils 폴더 내의 index.ts 파일
-export { default as selectColorForStatus } from "./selectColorForStatus";
-export { default as selectBackgroundColorForStatus } from "./selectBackgroundColorForStatus";
